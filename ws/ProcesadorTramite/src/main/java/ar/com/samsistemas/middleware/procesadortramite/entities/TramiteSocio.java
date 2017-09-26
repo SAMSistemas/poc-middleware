131713dc925ca9e8e6f12a69691b42a9149ffae7
Federico <fcatinello@gmail.com>
2017-09-26 01:05:04 -0300

    Upload Procesador y Notificador Tramites

--- ws/ProcesadorTramite/src/main/java/ar/com/samsistemas/middleware/procesadortramite/entities/TramiteSocio.java
+++ ws/ProcesadorTramite/src/main/java/ar/com/samsistemas/middleware/procesadortramite/entities/TramiteSocio.java
@@ -1,11 +1,16 @@
 package ar.com.samsistemas.middleware.procesadortramite.entities;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "tramite")
 public class TramiteSocio {
 
     private String socioContrato;
     private String tipo;
     private String descripcion;
 
+    @XmlElement(name = "socio")
     public String getSocioContrato() {
         return socioContrato;
     }
@@ -14,6 +19,7 @@ public class TramiteSocio {
         this.socioContrato = socioContrato;
     }
 
+    @XmlElement
     public String getTipo() {
         return tipo;
     }
@@ -22,6 +28,7 @@ public class TramiteSocio {
         this.tipo = tipo;
     }
 
+    @XmlElement
     public String getDescripcion() {
         return descripcion;
     }
--- ws/ProcesadorTramite/src/main/java/ar/com/samsistemas/middleware/procesadortramite/routebuilder/ProcesadorTramiteRouteBuilder.java
+++ ws/ProcesadorTramite/src/main/java/ar/com/samsistemas/middleware/procesadortramite/routebuilder/ProcesadorTramiteRouteBuilder.java
@@ -1,13 +1,7 @@
 package ar.com.samsistemas.middleware.procesadortramite.routebuilder;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.ejb.Startup;
-import javax.enterprise.context.ApplicationScoped;
 
 import static org.apache.activemq.camel.component.ActiveMQComponent.*;
 
@@ -15,39 +9,39 @@ import ar.com.samsistemas.middleware.procesadortramite.entities.TramiteSocio;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.cdi.ContextName;
 import org.apache.camel.model.dataformat.JaxbDataFormat;
 
-
-@Startup
-@ApplicationScoped
-@ContextName("ProcesadorTramite-context")
 public class ProcesadorTramiteRouteBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() {
 
-		// Conecto con la instancia de ActiveMQ
+		/** Bind JMS connection to camel context **/
+
 		getContext().addComponent("activemq", activeMQComponent("tcp://localhost:61616"));
+
+		/** Define XML to POJO dataformat **/
 
 		JaxbDataFormat dataFormat = new JaxbDataFormat(true);
 		dataFormat.setContextPath(TramiteSocio.class.getPackage().getName());
 		dataFormat.setPartClass(TramiteSocio.class.getName());
 
-		// Manejo de excepciones
+		/** Exception handling **/
+
 		onException(Exception.class)
 			.log(LoggingLevel.ERROR, "ar.com.samsistemas.services.tramites.routebuilder", "ProcesadorTramite exception: ${exception}")
 			.handled(true);
 
-		//Obtengo mensaje de la cola MQ correspondiente a un tramite
+		/** Get message from AMQ and insert into DB **/
+
 		from("activemq:TRAMITES")
 			.unmarshal(dataFormat)
 			.process(this::procesarTramite)
-			.to("sql:"+getQuery()+"?dataSource=java:jboss/jdbc/mysql");
+			.to("sql:"+insertQuery()+"?dataSource=mysqlDS");
 
 	}
 
-	private String getQuery(){
+	private String insertQuery(){
 		return "INSERT INTO poc_middleware.TramiteSocio(tipo,descripcion,estado,timestamp,id_socio) " +
 				"VALUES (:#tipo,:#descripcion,:#estado, NOW(), (SELECT id FROM poc_middleware.Socio s WHERE s.contrato = :#contrato_socio))";
 	}
@@ -59,7 +53,7 @@ public class ProcesadorTramiteRouteBuilder extends RouteBuilder {
 		Map<String, Object> queryParameters = new HashMap<>();
 		queryParameters.put("tipo", tramite.getTipo());
 		queryParameters.put("descripcion", tramite.getDescripcion());
-		queryParameters.put("estado", (Math.random()>0.5)?"ABIERTO":"CERRADO");
+		queryParameters.put("estado", (Math.random()>0.5)?"RECHAZADO":"CERRADO");
 		queryParameters.put("contrato_socio", tramite.getSocioContrato());
 
 		exchange.getOut().setBody(queryParameters);
